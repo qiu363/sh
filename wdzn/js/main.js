020c28ba6d73f138e9a6cde2c5bc1c241599fba3
jqliu <junqiu.liu@tuotuo-inc.com>
2021-04-23 16:25:23 +0800

    add

--- wdzn/js/main.js
+++ wdzn/js/main.js
@@ -6,21 +6,22 @@ var swiper = new Swiper('.swiper-container', {
 });
 
 $(window).bind('scroll', function () {
-  var winHeight = $(window).height();
   var scoTop = $(window).scrollTop();
+  var heaHeight = $('.header').height();
 
   var cur = 0
   $('.J_title').each(function () {
     var top = $(this).offset().top;
-    if (top < winHeight + scoTop + 20) {
-      cur = $('.J_title').index(this)
+    if (top < scoTop + heaHeight + 10) {
+      cur = $('.J_title').index(this) + 1
     }
   });
   $('#J_nav a').siblings().removeClass('cur').eq(cur).addClass('cur');
 });
 
-$('#J_nav a').click(function () {
+$('#J_nav a, #J_nav_f span').click(function () {
   var mod = $(this).attr('data-mod');
+  var heaHeight = $('.header').height();
 
   if (!mod) {
     $('html, body').animate({
@@ -32,12 +33,21 @@ $('#J_nav a').click(function () {
       if (mod === tmod) {
         var top = $(this).offset().top;
         $('html, body').animate({
-          scrollTop: top - 140
+          scrollTop: top - heaHeight
         }, 500);
       }
     });
   }
 
-
   return false;
+});
+
+$('#J_menu').click(function () {
+  if ($(this).hasClass('menu-open')) {
+    $('#J_nav').removeClass('nav-open');
+    $(this).removeClass('menu-open');
+  } else {
+    $('#J_nav').addClass('nav-open');
+    $(this).addClass('menu-open');
+  }
 });
